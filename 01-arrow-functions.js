09b9d2ed63a7aed0078c72b9299af92b7c21c828
´´xsebasti´´ <caballerorosebas@gmail.com>
2025-04-08 18:14:32 -0500

    vamos-x2

--- 01-arrow-functions.js
+++
@@ -1,6 +0,0 @@
-const multiplo = (numero) => {
-    return numero * numero
- } // de manera explicita retornando la expreesion y haciendo el codigo mas largo
-
- const suma = numero => numero + numero
- // de manera implicita lo que hace la linea mas corta y mas legible
