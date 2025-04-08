6ac9a3018f4256234eb0f1024beaefe0f1d581cc
´´xsebasti´´ <caballerorosebas@gmail.com>
2025-04-08 18:04:36 -0500

    vamos-que-vamos

--- 05-objectosLiterals.js
+++
@@ -1,8 +0,0 @@
-function nuevaPersona(nombre, edad, ciudad){
-    return {
-        nombre : nombre,
-        edad : edad,
-        ciudad : ciudad
-    }
-}
-    console.log(nuevaPersona("sebas", 17, "envigado"))
